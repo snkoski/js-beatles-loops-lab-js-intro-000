8d5000b754d3ff2b11d519eb04b2eb00224b04e6
Shawn Koski <shawnnkoski@gmail.com>
2018-03-05 23:46:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(musicians, instruments) {
+  var theBand = [];
+  for(var i = 0; i < musicians.length; i++) {
+    theBand.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return theBand;
+}
+
+const factsArray = ["He was the last Beatle to learn to drive", "He was never a vegetartian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+
+function johnLennonFacts(factsArray) {
+  var excitedFacts = [];
+  while (excitedFacts.length !== factsArray.length) {
+    excitedFacts.push()
+  }
+}
